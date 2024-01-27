e4b9110fea57a9d273148e2f4bcf5760bce583d5
Rémi Monthiller <monthir@git.inpt.fr>
2024-01-27 14:09:17 +0100

    apple

--- modules/remi/pigeon/pigeon.gd
+++ modules/remi/pigeon/pigeon.gd
@@ -8,6 +8,9 @@ extends Node2D
 var state_left: String = "idle"
 var state_right: String = "idle"
 
+var score_left: int = 0
+var score_right: int = 0
+
 func stun_left():
 	state_left = "stunned"
 	$TimerStunLeft.start()
@@ -19,6 +22,14 @@ func stun_right():
 func die():
 	player_keyboard_left.queue_free()
 	player_keyboard_right.queue_free()
+	# corps
+	var new_corps: Node2D = preload("res://modules/remi/pigeon/pigeon_corps.tscn").instantiate()
+	new_corps.position = transform * $Body.position
+	get_parent().add_child(new_corps)
+	# warn game of death
+	var game: Node = get_parent().get_parent()
+	game.on_pigeon_just_died()
+	# free
 	queue_free()
 
 # internal
--- modules/remi/walker/foot.gd
+++ modules/remi/walker/foot.gd
@@ -19,6 +19,7 @@ func end_step():
 	# activating FootBody
 	$FootBody.show()
 	$FootBody.process_mode = Node.PROCESS_MODE_INHERIT
+	# empty
 	if not overlapping_bodies.is_empty():
 		var pigeon: Node2D = overlapping_bodies.front().get_parent()
 		for body in overlapping_bodies:
@@ -26,14 +27,40 @@ func end_step():
 				"ForearmLeft":
 					pigeon.stun_left()
 					if pigeon.state_left == "hitting":
-						# TODO: hit animation
+						$FootBody.process_mode = Node.PROCESS_MODE_DISABLED
+						# free
 						queue_free()
 				"ForearmRight":
 					pigeon.stun_right()
 					if pigeon.state_right == "hitting":
-						# TODO: hit animation
+						$FootBody.process_mode = Node.PROCESS_MODE_DISABLED
+						# anim
+						## init
+						$FootBody/Sprite2D2.hide()
+						$FootBody/Sprite2D2HitDove.show()
+						## tween
+						var tween: Tween = create_tween()
+						tween.tween_property($FootBody/Sprite2DHitDove, "rotation", -0.125*PI, 0.125).set_trans(Tween.TRANS_CUBIC)
+						tween.tween_property($FootBody/Sprite2DHitDove, "rotation", 0.125*PI, 0.125).set_trans(Tween.TRANS_CUBIC)
+						tween.tween_property($FootBody/Sprite2DHitDove, "rotation", -0.125*PI, 0.125).set_trans(Tween.TRANS_CUBIC)
+						tween.tween_property($FootBody/Sprite2DHitDove, "rotation", 0.125*PI, 0.125).set_trans(Tween.TRANS_CUBIC)
+						tween.tween_property($FootBody/Sprite2DHitDove, "rotation", -0.125*PI, 0.125).set_trans(Tween.TRANS_CUBIC)
+						tween.tween_property($FootBody/Sprite2DHitDove, "rotation", 0.125*PI, 0.125).set_trans(Tween.TRANS_CUBIC)
+						tween.tween_property($FootBody/Sprite2DHitDove, "rotation", -0.125*PI, 0.125).set_trans(Tween.TRANS_CUBIC)
+						tween.tween_property($FootBody/Sprite2DHitDove, "rotation", 0.125*PI, 0.125).set_trans(Tween.TRANS_CUBIC)
+						tween.tween_property($FootBody/Sprite2DHitDove, "modulate:a", 0.0, 0.25).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+						await tween.finished
+						# free
 						queue_free()
 				"Body":
 					body.get_parent().die()
 				_:
 					push_error("body name not recognised")
+
+func _play_hit_pigeon_animation():
+	## init
+	$FootBody/Sprite2D2HitPigeon.show()
+	## tween
+	var tween: Tween = create_tween()
+	tween.tween_property($FootBody/Sprite2DHitDove, "modulate:a", 0.0, 0.5).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+	await tween.finished
